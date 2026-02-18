a35e350ff5dd33fbbdef795ef74345980b52b217
Sanjay Ramabuddi <sanjayramabuddi@gmail.com>
2026-02-18 12:49:52 +0530

    modified routing to show hompage for all users

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Body from "./pages/Body";
-import Login from "./pages/Login";
-import store from "./utils/store/store";
 import { Provider } from "react-redux";
+
+import Body from "./pages/Body";
+import AuthLayout from "./layouts/AuthLayout";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import HomePage from "./pages/HomePage";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
 
+import "./App.css";
+
+import store from "./utils/store/store";
+
 const App = () => {
   return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Body />}>
-              <Route path="/" element={<Feed />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/profile" element={<Profile />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Body />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
