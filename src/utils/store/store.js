da9634306cfd8026fc275c567a1c277ac197f2c9
Sanjay Ramabuddi <sanjayramabuddi@gmail.com>
2026-02-21 22:54:00 +0530

    created slices for connections and requests

--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import connectionReducer from "./feedSlice";
+import requestReducer from "./feedSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connection: connectionReducer,
+    request: requestReducer,
   },
 });
 
