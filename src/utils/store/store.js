24e3ac614e5cb01ce046218d74deda9bb1f8e536
Sanjay Ramabuddi <sanjayramabuddi@gmail.com>
2026-03-04 18:01:29 +0530

    fixed the slice imports

--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
-import connectionReducer from "./feedSlice";
-import requestReducer from "./feedSlice";
+import connectionReducer from "./connectionSlice";
+import requestReducer from "./requestSlice";
 
 const store = configureStore({
   reducer: {
